8fc43c082cdf13855edc3f9e2f49d3a8c045c8d6
yelu <laotianye01@gmail.com>
2024-05-15 19:58:22 +0800

    deposit and withdraw commit

---
+++ src/main/java/View/DepositWithdraw/PersonalPage.java
@@ -0,0 +1,168 @@
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import com.intellij.uiDesigner.core.*;
+/*
+ * Created by JFormDesigner on Wed May 15 16:24:58 GMT+08:00 2024
+ */
+
+
+
+/**
+ * @author 86166
+ */
+public class PersonalPage extends JPanel {
+    public PersonalPage() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        ResourceBundle bundle = ResourceBundle.getBundle("form");
+        panel1 = new JPanel();
+        button1 = new JButton();
+        button2 = new JButton();
+        button3 = new JButton();
+        button4 = new JButton();
+        button5 = new JButton();
+        textField1 = new JTextField();
+        textField2 = new JTextField();
+        button6 = new JButton();
+        button7 = new JButton();
+        button8 = new JButton();
+        button9 = new JButton();
+        textField3 = new JTextField();
+        button10 = new JButton();
+
+        //======== this ========
+        setPreferredSize(new Dimension(1200, 900));
+        setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+
+        //======== panel1 ========
+        {
+            panel1.setPreferredSize(new Dimension(1200, 900));
+            panel1.setOpaque(false);
+
+            //---- button1 ----
+            button1.setText(bundle.getString("PersonalPage.button1.text"));
+            button1.setFont(new Font("Microsoft YaHei UI", Font.PLAIN, 30));
+
+            GroupLayout panel1Layout = new GroupLayout(panel1);
+            panel1.setLayout(panel1Layout);
+            panel1Layout.setHorizontalGroup(
+                panel1Layout.createParallelGroup()
+                    .addGroup(panel1Layout.createSequentialGroup()
+                        .addGap(46, 46, 46)
+                        .addComponent(button1, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(1018, Short.MAX_VALUE))
+                    .addGroup(panel1Layout.createSequentialGroup()
+                        .addGap(101, 101, 101)
+                        .addGroup(panel1Layout.createParallelGroup()
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addComponent(button2, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+                                .addGap(167, 167, 167)
+                                .addComponent(button3)
+                                .addGap(142, 142, 142)
+                                .addComponent(button4)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 179, Short.MAX_VALUE)
+                                .addComponent(button5)
+                                .addGap(127, 127, 127))
+                            .addGroup(GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
+                                .addGroup(panel1Layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                    .addGroup(panel1Layout.createSequentialGroup()
+                                        .addComponent(textField3, GroupLayout.PREFERRED_SIZE, 689, GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 249, Short.MAX_VALUE)
+                                        .addComponent(button10, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(panel1Layout.createSequentialGroup()
+                                        .addGroup(panel1Layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                            .addGroup(panel1Layout.createSequentialGroup()
+                                                .addComponent(textField2, GroupLayout.PREFERRED_SIZE, 689, GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
+                                                .addComponent(button8, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE))
+                                            .addGroup(panel1Layout.createSequentialGroup()
+                                                .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 689, GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
+                                                .addComponent(button7, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)))
+                                        .addGroup(panel1Layout.createParallelGroup()
+                                            .addGroup(panel1Layout.createSequentialGroup()
+                                                .addGap(67, 67, 67)
+                                                .addComponent(button9, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE))
+                                            .addGroup(GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
+                                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(button6, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)))))
+                                .addGap(56, 56, 56))))
+            );
+            panel1Layout.setVerticalGroup(
+                panel1Layout.createParallelGroup()
+                    .addGroup(panel1Layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addComponent(button1, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+                        .addGroup(panel1Layout.createParallelGroup()
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addGap(172, 172, 172)
+                                .addComponent(button3, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE))
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addGap(172, 172, 172)
+                                .addGroup(panel1Layout.createParallelGroup()
+                                    .addComponent(button4, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(button5, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addGap(83, 83, 83)
+                                .addComponent(button2, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)))
+                        .addGap(154, 154, 154)
+                        .addGroup(panel1Layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                            .addComponent(textField3, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(button10, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE))
+                        .addGap(42, 42, 42)
+                        .addGroup(panel1Layout.createParallelGroup()
+                            .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addGroup(panel1Layout.createParallelGroup()
+                                    .addComponent(button7, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(button6, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE))))
+                        .addGroup(panel1Layout.createParallelGroup()
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addGap(49, 49, 49)
+                                .addComponent(textField2, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE))
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addGap(58, 58, 58)
+                                .addGroup(panel1Layout.createParallelGroup()
+                                    .addComponent(button9, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(button8, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE))))
+                        .addContainerGap(32, Short.MAX_VALUE))
+            );
+        }
+        add(panel1, new GridConstraints(0, 0, 1, 1,
+            GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
+            null, null, null));
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JTextField textField3;
+    private JButton button10;
+
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.repaint();
+        ImageIcon image = new ImageIcon("src/main/imgs/PersonalPage.png");
+        g.drawImage(image.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+    }
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
